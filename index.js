9a95972e47aff8f6de1b25c08a8c23beadffb6e6
Heath123 <heath.mitchell27@gmail.com>
2021-01-02 13:13:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/* global THREE, fetch */
+
+// Workaround for process.versions.node not existing in the browser
+process.versions.node = '14.0.0'
+
+const mineflayer = require('mineflayer')
+const { WorldView, Viewer, MapControls } = require('prismarine-viewer/viewer')
+const { Vec3 } = require('vec3')
+global.THREE = require('three')
+
+async function main () {
+  const version = '1.16.4'
+
+  const viewDistance = 6
+
+  const bot = mineflayer.createBot({
+    host: '95.111.249.143',
+    port: 10000,
+    username: 'echo',
+  })
+
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    bot.chat(message)
+  })
+
+  bot.once('spawn', () => {
+    console.log('bot spawned - starting viewer')
+
+    const center = bot.entity.position
+
+    const worldView = new WorldView(bot.world, viewDistance, center)
+
+    // Create three.js context, add to page
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setPixelRatio(window.devicePixelRatio || 1)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    // Create viewer
+    const viewer = new Viewer(renderer)
+    viewer.setVersion(version)
+    // Attach controls to viewer
+    const controls = new MapControls(viewer.camera, renderer.domElement)
+
+    worldView.listenToBot(bot)
+
+    function botPosition () {
+      worldView.updatePosition(bot.entity.position)
+      viewer.camera.position.set(bot.entity.position.x, bot.entity.position.y, bot.entity.position.z)
+    }
+
+    bot.on('move', botPosition)
+
+    // Link WorldView and Viewer
+    viewer.listen(worldView)
+
+    viewer.camera.position.set(center.x, center.y, center.z)
+    controls.update()
+
+    // Browser animation loop
+    const animate = () => {
+      window.requestAnimationFrame(animate)
+      if (controls) controls.update()
+      // worldView.updatePosition(controls.target)
+      worldView.updatePosition(bot.entity.position)
+      renderer.render(viewer.scene, viewer.camera)
+    }
+    animate()
+  })
+}
+main()
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack')
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const config = {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  entry: path.resolve(__dirname, './index.js'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: './index.js'
+  },
+  resolve: {
+    alias: {
+      "minecraft-protocol": path.resolve(
+        __dirname,
+        "node_modules/minecraft-protocol/src/index.js"
+      ), // Hack to allow creating the client in a browser
+      "express": false,
+      "net": "net-browserify",
+      "fs": "memfs"
+    },
+    fallback: {
+      zlib: require.resolve('browserify-zlib'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+      events: require.resolve('events/'),
+      assert: require.resolve('assert/'),
+      crypto: require.resolve("crypto-browserify"),
+      path: require.resolve("path-browserify"),
+      constants: require.resolve("constants-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      http: require.resolve("http-browserify"),
+      https: require.resolve("https-browserify"),
+      timers: require.resolve("timers-browserify"),
+      // fs: require.resolve("fs-memory/singleton"),
+      child_process: false,
+      perf_hooks: path.resolve(__dirname, "perf_hooks_replacement.js"),
+      dns: path.resolve(__dirname, "dns.js")
+  }
+  },
+  plugins: [
+    // fix "process is not defined" error:
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    }),
+    new webpack.NormalModuleReplacementPlugin(
+      /prismarine-viewer[/|\\]viewer[/|\\]lib[/|\\]utils/,
+      './utils.web.js'
+    )/*,
+    new CopyPlugin({
+      patterns: [
+        { from: '../../public/blocksStates/', to: './blocksStates/' },
+        { from: '../../public/textures/', to: './textures/' },
+        { from: '../../public/worker.js', to: './' },
+        { from: '../../public/supportedVersions.json', to: './' }
+      ]
+    })*/
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    compress: true,
+    inline: true,
+    // open: true,
+    hot: true,
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  }
+}
+
+module.exports = config
